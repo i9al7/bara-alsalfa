fc901f476fd5f4c6d0259354a039317aed46f3fc
saleh alkharraz <106388788+i9al7@users.noreply.github.com>
2026-05-19 23:31:01 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
